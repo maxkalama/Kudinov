660ebc82089c943c7e1aa4099304f83eb8940518
Max Kudinov <maxkalama@gmail.com>
2022-07-06 14:48:25 +0300

    Add project files.

---
+++ VeeamTest/FileSignatureGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeeamTest
+{
+
+    static class FileSignatureGenerator
+    {
+        public static void Generate(int blockSize, string filePath)
+        {
+            var stream = File.OpenRead(filePath);            
+        }
+    }
+}
---
+++ VeeamTest/Program.cs
@@ -0,0 +1,20 @@
+﻿int blockSize = 0;
+string? filePath = null;
+
+if (args.Length != 2)
+    Console.WriteLine("Usage: VeeamTest [block size] [file path]");
+
+if (!int.TryParse(args[0], out blockSize) || blockSize <= 0)
+    Console.WriteLine("Wrong block size. A positive integer is expected.");
+
+if (!File.Exists(filePath)) //checks file path for null or zero-length
+    Console.WriteLine("Wrong file path. A path to an existing read-accessable file is expected.");
+
+try
+{
+    VeeamTest.FileSignatureGenerator.Generate(blockSize, filePath);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
